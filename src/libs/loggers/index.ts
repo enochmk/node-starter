cce75275274c5b6e0c31908867a1c216dec2b1d1
Enoch Klufio <Enochmk94@gmail.com>
2023-03-14 09:47:46 +0000

    refactor: update logger lib

--- src/libs/loggers/index.ts
+++ src/libs/loggers/index.ts
@@ -1,15 +1,61 @@
 import config from 'config';
-import winston from 'winston';
-import consoleTransport from './transports/console.transport';
-import fileTransport from './transports/file.transport';
+import winston, { format } from 'winston';
+import rtracer from 'cls-rtracer';
+import 'winston-daily-rotate-file';
 
 const service = config.get('logger.service');
-const transports = [consoleTransport, fileTransport];
+const dirname = config.get('logger.dirname') as string;
+const LEVEL = config.get('logger.level') as string;
 const TIMESTAMP_FORMAT = 'YYYY-MM-DD HH:mm:ss';
+
+/* ---------------------------------------- */
+// log format
+const fileLogFormat = format.printf((log: any): string => {
+  const requestID = rtracer.id();
+  const { message, level, timestamp, ...rest } = log;
+  return JSON.stringify({
+    timestamp: timestamp,
+    requestID: requestID,
+    level: level,
+    message: message,
+    metadata: rest,
+  });
+});
+
+// File transport configuration
+const fileTransport = new winston.transports.DailyRotateFile({
+  level: LEVEL,
+  dirname,
+  datePattern: 'YYYYMMDD',
+  filename: '%DATE%.log',
+  format: winston.format.combine(fileLogFormat),
+});
+
+/* ---------------------------------------- */
+// log format
+const consoleLogFormat = format.printf((log: any): string => {
+  const { timestamp, level, message, label, ...rest } = log;
+
+  // if label is set, add it to the log message
+  if (label) {
+    return `[${timestamp}] [${level?.toUpperCase()}]: [${label}] - ${message} ${
+      Object.keys(rest).length ? JSON.stringify(rest) : ''
+    }`;
+  }
+
+  return `[${timestamp}] [${level?.toUpperCase()}]: ${message} ${Object.keys(rest).length ? JSON.stringify(rest) : ''}`;
+});
+
+// Console transport configuration
+const consoleTransport = new winston.transports.Console({
+  level: LEVEL,
+  format: winston.format.combine(consoleLogFormat, winston.format.colorize({ all: true })),
+});
+/* ---------------------------------------- */
 
 export const getLogger = (label: string) => {
   return winston.createLogger({
-    transports: transports,
+    transports: [consoleTransport, fileTransport],
     levels: winston.config.npm.levels,
     defaultMeta: {
       label: label,
@@ -21,9 +67,9 @@ export const getLogger = (label: string) => {
   });
 };
 
-// configure logger with label
+//* configure logger with label
 const logger = winston.createLogger({
-  transports: transports,
+  transports: [consoleTransport, fileTransport],
   levels: winston.config.npm.levels,
   defaultMeta: { service },
   format: winston.format.combine(
